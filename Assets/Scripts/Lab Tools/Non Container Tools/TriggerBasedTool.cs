5fa906aa089ac293eaf06ea939d626de874e2912
biobinary <mmcury2019@gmail.com>
2025-05-05 00:40:46 +0700

    Fix the damn trigger based tool bug

--- Assets/Scripts/Lab Tools/Non Container Tools/TriggerBasedTool.cs
+++ Assets/Scripts/Lab Tools/Non Container Tools/TriggerBasedTool.cs
@@ -6,65 +6,33 @@ public abstract class TriggerBasedTool : MonoBehaviour {
 
 	[SerializeField] protected GrabInteractable m_mainGrabbable;
 
-	private bool m_isSelectedByInteractor = false;
 	private bool m_wasTriggerPressedLastFrame = false;
-
-	private void OnEnable() {
-		m_mainGrabbable.WhenSelectingInteractorViewAdded += OnHandleSelectedByInteractor;
-		m_mainGrabbable.WhenSelectingInteractorViewRemoved += OnHandleUnselectedByInteractor;
-	}
-
-	private void OnDisable() {
-		m_isSelectedByInteractor = false;
-		m_mainGrabbable.WhenSelectingInteractorViewAdded -= OnHandleSelectedByInteractor;
-		m_mainGrabbable.WhenSelectingInteractorViewRemoved -= OnHandleUnselectedByInteractor;
-
-	}
-
-	private void OnHandleSelectedByInteractor(IInteractorView interactor) {
-		m_isSelectedByInteractor = true;
-	}
-
-	private void OnHandleUnselectedByInteractor(IInteractorView interactor) {
-		m_isSelectedByInteractor = false;
-	}
 
 	protected virtual void Update() {
 
-		if (m_isSelectedByInteractor) {
+		bool isPressed = false;
 
-			bool isPressed = false;
+		foreach (IInteractorView interactorView in m_mainGrabbable.SelectingInteractorViews) {
 
-			foreach (IInteractorView interactorView in m_mainGrabbable.SelectingInteractorViews) {
-				
-				object interactorData = interactorView.Data;
-				
-				if (interactorData is GrabInteractor grabInteractor) {
-					ControllerRef currentController = grabInteractor.GetComponent<ControllerRef>();
-					isPressed = currentController.ControllerInput.TriggerButton;
-					break;
-				}
+			object interactorData = interactorView.Data;
 
+			if (interactorData is GrabInteractor grabInteractor) {
+				ControllerRef currentController = grabInteractor.GetComponent<ControllerRef>();
+				isPressed = currentController.ControllerInput.TriggerButton;
+				break;
 			}
-			
-			if (isPressed && !m_wasTriggerPressedLastFrame) {
-				OnHandleTriggerPressed();
-
-			} else if (!isPressed && m_wasTriggerPressedLastFrame) {
-				OnHandleTriggerReleased();
-			
-			}
-
-			m_wasTriggerPressedLastFrame = isPressed;
-		
-		} else {
-
-			if (m_wasTriggerPressedLastFrame)
-				OnHandleTriggerReleased();
-
-			m_wasTriggerPressedLastFrame = false;
 
 		}
+
+		if (isPressed && !m_wasTriggerPressedLastFrame) {
+			OnHandleTriggerPressed();
+
+		} else if (!isPressed && m_wasTriggerPressedLastFrame) {
+			OnHandleTriggerReleased();
+
+		}
+
+		m_wasTriggerPressedLastFrame = isPressed;
 
 	}
 
